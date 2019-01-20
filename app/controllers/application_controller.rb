d0a680ac3affd18dfffed94722c586e210dc43b4
Russell <rgee258@gmail.com>
2019-01-20 18:54:33 -0500

    Project completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :logged_in?
+
+  def log_in(user)
+    remember_token = User.digest(User.new_token)
+    user.update_attribute(:remember_token, remember_token)
+    cookies.permanent.signed[:remember_token] = remember_token
+    current_user = user
+  end
+
+  def log_out
+    current_user.update_attribute(:remember_token, nil)
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Handling the current user
+
+  def current_user
+    if cookies[:remember_token]
+      @current_user ||= User.find_by(remember_token: cookies.signed[:remember_token])
+    end
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
+
+  before_create :remember
+
   has_secure_password
+
+  class << self
+    # Create a token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # Convert to digest
+    def digest(string)
+      Digest::SHA1.hexdigest(string.to_s)
+    end
+  end
+
+  def remember
+    self.remember_token = User.digest(User.new_token)
+  end
 end
